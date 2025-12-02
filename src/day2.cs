8dce48b2c3c2ced435d76ffa7ac1f443e27094ea
Marc Audy <MarcAudy@users.noreply.github.com>
2025-12-01 23:28:02 -0800

    Day 2 consolidate solution

--- src/day2.cs
+++ src/day2.cs
@@ -4,49 +4,38 @@ class day2
 {
     public static void run()
     {
-        Console.WriteLine($"Day2 Part1 Example: {part1("day2_example")}");
-        Console.WriteLine($"Day2 Part1: {part1("day2_input")}");
-        Console.WriteLine($"Day2 Part2 Example: {part2("day2_example")}");
-        Console.WriteLine($"Day2 Part2: {part2("day2_input")}");
+        var exampleResult = evaluate("day2_example");
+        var result = evaluate("day2_input");
+        Console.WriteLine($"Day2 Part1 Example: {exampleResult.Item1}");
+        Console.WriteLine($"Day2 Part1: {result.Item1}");
+        Console.WriteLine($"Day2 Part2 Example: {exampleResult.Item2}");
+        Console.WriteLine($"Day2 Part2: {result.Item2}");
     }
 
-    private static ulong part1(string fileName)
+    private static (ulong, ulong) evaluate(string fileName)
     {
-        ulong invalidIDSum = 0;
+        ulong part1_invalidIDSum = 0;
+        ulong part2_invalidIDSum = 0;
         FileProcessor.ProcessSegments(fileName, ',', segment =>
         {
             string[] segments = segment.Split('-');
             ulong rangeStart = ulong.Parse(segments[0]);
             ulong rangeEnd = ulong.Parse(segments[1]);
-            Regex regex = new(@"^(.+)\1$");
+            Regex part1_regex = new(@"^(.+)\1$");
+            Regex part2_regex = new(@"^(.+)\1+$");
             for (ulong id = rangeStart; id <= rangeEnd; id++)
             {
-                if (regex.Match($"{id}").Success)
+                if (part1_regex.Match($"{id}").Success)
                 {
-                    invalidIDSum += id;
+                    part1_invalidIDSum += id;
+                    part2_invalidIDSum += id;
+                }
+                else if (part2_regex.Match($"{id}").Success)
+                {
+                    part2_invalidIDSum += id;
                 }
             }
         });
-        return invalidIDSum;
-    }
-
-    private static ulong part2(string fileName)
-    {
-        ulong invalidIDSum = 0;
-        FileProcessor.ProcessSegments(fileName, ',', segment =>
-        {
-            string[] segments = segment.Split('-');
-            ulong rangeStart = ulong.Parse(segments[0]);
-            ulong rangeEnd = ulong.Parse(segments[1]);
-            Regex regex = new(@"^(.+)\1+$");
-            for (ulong id = rangeStart; id <= rangeEnd; id++)
-            {
-                if (regex.Match($"{id}").Success)
-                {
-                    invalidIDSum += id;
-                }
-            }
-        });
-        return invalidIDSum;
+        return (part1_invalidIDSum, part2_invalidIDSum);
     }
 }
