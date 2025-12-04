93a51de6d7536922f67fe7a8fd11e813b8682855
Marc Audy <MarcAudy@users.noreply.github.com>
2025-12-04 09:13:20 -0800

    Day2 and 4x speed by using parallel for

--- src/day2.cs
+++ src/day2.cs
@@ -1,27 +1,33 @@
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 partial class Day2
 {
     public static void Run()
     {
+        Stopwatch sw = Stopwatch.StartNew();        
         var exampleResult = Evaluate("day2_example");
         var result = Evaluate("day2_input");
+        sw.Stop();
+
         Console.WriteLine($"Day2 Part1 Example: {exampleResult.part1}");
         Console.WriteLine($"Day2 Part1: {result.part1}");
         Console.WriteLine($"Day2 Part2 Example: {exampleResult.part2}");
         Console.WriteLine($"Day2 Part2: {result.part2}");
+        Console.WriteLine($"Solved Day 2 in {sw.Elapsed.TotalSeconds} seconds");
+
     }
 
-    private static (ulong part1, ulong part2) Evaluate(string fileName)
+    private static (long part1, long part2) Evaluate(string fileName)
     {
-        ulong part1_invalidIDSum = 0;
-        ulong part2_invalidIDSum = 0;
-        FileProcessor.ProcessSegments(fileName, ',', segment =>
+        long part1_invalidIDSum = 0;
+        long part2_invalidIDSum = 0;
+        FileProcessor.ParallelProcessSegments(fileName, ',', segment =>
         {
             string[] segments = segment.Split('-');
-            ulong rangeStart = ulong.Parse(segments[0]);
-            ulong rangeEnd = ulong.Parse(segments[1]);
-            for (ulong id = rangeStart; id <= rangeEnd; id++)
+            long rangeStart = long.Parse(segments[0]);
+            long rangeEnd = long.Parse(segments[1]);
+            Parallel.For(rangeStart, rangeEnd+1, id =>
             {
                 if (part1_regex().Match($"{id}").Success)
                 {
@@ -32,7 +38,7 @@ partial class Day2
                 {
                     part2_invalidIDSum += id;
                 }
-            }
+            });
         });
         return (part1_invalidIDSum, part2_invalidIDSum);
     }
--- src/utils.cs
+++ src/utils.cs
@@ -1,16 +1,22 @@
 class FileProcessor
 {
+    public static string[] GetLines(string fileName)
+    {
+        return File.ReadAllLines($"../../../../input/{fileName}.txt");
+    }
+
     public static void ProcessLines(string fileName, Action<string> lineAction)
     {
-        foreach (string line in File.ReadAllLines($"../../../../input/{fileName}.txt"))
+        foreach (string line in GetLines(fileName))
         {
             lineAction(line);
         }
     }
 
-    public static string[] GetLines(string fileName)
+    public static void ParallelProcessLines(string fileName, Action<string> lineAction)
     {
-        return File.ReadAllLines($"../../../../input/{fileName}.txt");
+        string[] lines = GetLines(fileName);
+        Parallel.For(0, lines.Length, index => lineAction(lines[index]));
     }
 
     public static void ProcessSegments(string fileName, char separator, Action<string> segmentAction)
@@ -19,5 +25,11 @@ class FileProcessor
         {
             segmentAction(segment);
         }
+    }
+
+    public static void ParallelProcessSegments(string fileName, char separator, Action<string> segmentAction)
+    {
+        string[] segments = File.ReadAllText($"../../../../input/{fileName}.txt").Split(separator);
+        Parallel.For(0, segments.Length, index => segmentAction(segments[index]));
     }
 }
