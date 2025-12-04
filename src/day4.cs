2efaf37f94ad5ad18130fd308d36cb185cf8ed84
Marc Audy <MarcAudy@users.noreply.github.com>
2025-12-04 09:12:22 -0800

    Day4 timing and small optimization

--- src/day4.cs
+++ src/day4.cs
@@ -1,15 +1,19 @@
-using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 class Day4
 {
     public static void Run()
     {
+        Stopwatch sw = Stopwatch.StartNew();        
         var exampleResult = Evaluate("day4_example");
         var result = Evaluate("day4_input");
+        sw.Stop();
+
         Console.WriteLine($"Day4 Part1 Example: {exampleResult.part1}");
         Console.WriteLine($"Day4 Part1: {result.part1}");
         Console.WriteLine($"Day4 Part2 Example: {exampleResult.part2}");
         Console.WriteLine($"Day4 Part2: {result.part2}");
+        Console.WriteLine($"Solved Day 4 in {sw.Elapsed.TotalSeconds} seconds");
     }
 
     private static (int part1, int part2) Evaluate(string fileName)
@@ -21,7 +25,7 @@ class Day4
 
         while (true)
         {
-            HashSet<(int, int)> reachable = [];
+            List<(int, int)> reachable = [];
             for (int y = 0; y < grid.Length; y++)
             {
                 for (int x = 0; x < grid[y].Length; x++)
