b3f1eaa93b11261756d32330531f6638657ad27d
Marc Audy <MarcAudy@users.noreply.github.com>
2025-12-05 08:22:27 -0800

    Day 5 Part 2

--- src/day5.cs
+++ src/day5.cs
@@ -11,15 +11,13 @@ class Day5
 
         Console.WriteLine($"Day5 Part1 Example: {exampleResult.part1}");
         Console.WriteLine($"Day5 Part1: {result.part1}");
-        //Console.WriteLine($"Day5 Part2 Example: {exampleResult.part2}");
-        //Console.WriteLine($"Day5 Part2: {result.part2}");
+        Console.WriteLine($"Day5 Part2 Example: {exampleResult.part2}");
+        Console.WriteLine($"Day5 Part2: {result.part2}");
         Console.WriteLine($"Solved Day5 in {sw.Elapsed.TotalSeconds} seconds");
     }
 
-    private static (int part1, int part2) Evaluate(string fileName)
+    private static (int part1, ulong part2) Evaluate(string fileName)
     {
-        int part1_fresh = 0;
-
         string[] lines = FileProcessor.GetLines(fileName);
 
         List<(ulong, ulong)> freshRanges = [];
@@ -33,9 +31,47 @@ class Day5
                 break;
             }
             string[] rangeEnds = lines[index].Split('-');
-            freshRanges.Add((ulong.Parse(rangeEnds[0]),ulong.Parse(rangeEnds[1])));
+            ulong rangeStart = ulong.Parse(rangeEnds[0]);
+            ulong rangeEnd = ulong.Parse(rangeEnds[1]);
+            bool addRange = true;
+
+            // Remove overlaps by checking existing ranges
+            for (int n = freshRanges.Count - 1; n >= 0; n--)
+            {
+                // There are 5 scenarios to consider
+                (ulong start, ulong end) = freshRanges[n];
+                
+                // The new range is entirely inside the existing range
+                if (rangeStart >= start && rangeEnd <= end)
+                {
+                    addRange = false;
+                    break;
+                }
+                // The existing range is entirely inside the new range
+                else if (rangeStart <= start && rangeEnd >= end)
+                {
+                    freshRanges.RemoveAt(n);
+                }
+                // The new range starts earlier and overlaps
+                else if (rangeStart < start && rangeEnd >= start)
+                {
+                    rangeEnd = start - 1;
+                }
+                // The new range extends further but overlaps at the start
+                else if (rangeEnd > end && rangeStart <= end)
+                {
+                    rangeStart = end + 1;
+                }
+                // Otherwise there is no overlap and nothing to do
+            }
+
+            if (addRange)
+            {
+                freshRanges.Add((rangeStart, rangeEnd));
+            }
         }
 
+        int part1_fresh = 0;
         for (; index < lines.Length; index++)
         {
             ulong ingredientID = ulong.Parse(lines[index]);
@@ -45,6 +81,12 @@ class Day5
             }
         }
 
-        return (part1_fresh, 0);
+        ulong part2_fresh = 0;
+        foreach ((ulong start, ulong end) in freshRanges)
+        {
+            part2_fresh += end - start + 1;
+        }
+
+        return (part1_fresh, part2_fresh);
     }
 }
